484a1de5da7a7701bbea39dd76325e43c3122e7a
Ostrich <theamazingostrich1231@gmail.com>
2025-02-15 14:46:38 -0700

    added close command

--- CalendarBot/main.py
+++ CalendarBot/main.py
@@ -7,7 +7,9 @@ load_dotenv()
 
 TOKEN = os.getenv("BOT_TOKEN")
 intents = discord.Intents.default()
+intents.message_content = True  # Enable message content intent
 bot = commands.Bot(command_prefix='!', intents=intents)
+
 
 @bot.event
 async def on_ready():
@@ -15,6 +17,12 @@ async def on_ready():
 
 @bot.command()
 async def ping(ctx):
+    print('User sent a command!')
     await ctx.send('Pong!')
+
+@bot.command()
+async def close(ctx):
+    print('User sent a command!')
+    await bot.close()
 
 bot.run(TOKEN)
