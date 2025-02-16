bb76b7dc68b964521ac9a6e468ad8b35837498ec
JohnNasitem <jnasitem1@nait.ca>
2025-02-15 21:02:40 -0700

    fixed dup ping issue and got basic image gen to work

--- CalendarBot/cogs/availabilities.py
+++ CalendarBot/cogs/availabilities.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from PIL import Image, ImageDraw, ImageFont
 
 class Availability(commands.Cog):
     def __init__(self, bot):
@@ -11,8 +12,20 @@ class Availability(commands.Cog):
 
     #replace with availability commands
     @commands.command()
-    async def ping(self, ctx):
+    async def cmd2(self, ctx):
         await ctx.send("Pong!!")
+
+    # replace with availability commands
+    @commands.command()
+    async def image(self, ctx):
+        img = Image.new('RGB', (200, 100), color = (255, 0, 0))
+        draw = ImageDraw.Draw(img)
+        img.save('generated_images/test.png')
+
+        with open('generated_images/test.png', 'rb') as f:
+            await ctx.send("Here is your image!",file=discord.File(f))
 
 async def setup(bot):
     await bot.add_cog(Availability(bot))
+
+
