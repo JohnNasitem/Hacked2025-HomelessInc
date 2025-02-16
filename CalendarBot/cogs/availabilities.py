3cc2cd4c107dd209af99d1919ed0dd8ec82fbbfd
JohnNasitem <jnasitem1@nait.ca>
2025-02-15 18:06:50 -0700

    added files for each feature

---
+++ CalendarBot/cogs/availabilities.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+
+class Availability(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("Bot is online!")
+
+    #replace with availability commands
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.send("Pong!!")
+
+async def setup(bot):
+    await bot.add_cog(Availability(bot))
---
+++ CalendarBot/cogs/events.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+
+class Event(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("Bot is online!")
+
+    #replace with events commands
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.send("Pong!!")
+
+async def setup(bot):
+    await bot.add_cog(Event(bot))
