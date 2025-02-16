6155a6bdcf8e1625a4c4a8157138b744b1455989
ThatOneOstrich <theamazingostrich1231@gmail.com>
2025-02-15 22:02:32 -0700

    fixed cogs not working

--- CalendarBot/main.py
+++ CalendarBot/main.py
@@ -17,7 +17,8 @@ async def on_ready():
     print(f'Logged in as {bot.user}')
 
 async def load():
-    for filename in os.listdir("./cogs"):
+    cogs_dir = os.path.join(os.path.dirname(__file__), 'cogs')
+    for filename in os.listdir(cogs_dir):
         if filename.endswith(".py"):
             await bot.load_extension(f'cogs.{filename[:-3]}')
 
