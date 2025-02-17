3a01bd13194f4c44e0f5bddceae4337404f22dbe
Ostrich <theamazingostrich1231@gmail.com>
2025-02-16 22:41:58 -0700

    pong!

--- CalendarBot/cogs/ping.py
+++ CalendarBot/cogs/ping.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from discord import app_commands
 
 class Ping(commands.Cog):
     def __init__(self, bot):
@@ -7,11 +8,12 @@ class Ping(commands.Cog):
 
     @commands.Cog.listener()
     async def on_ready(self):
-        print("Bot is online!")
+        print("Ping is online!")
 
-    @commands.command()
-    async def ping(self, ctx):
-        await ctx.send("Pong!!")
+    @app_commands.command()
+    async def ping(self, interaction: discord.Interaction):
+        latency = self.bot.latency * 1000  # Convert latency to milliseconds
+        await interaction.response.send_message(f"Pong! Latency: {latency:.2f} ms")
 
 async def setup(bot):
     await bot.add_cog(Ping(bot))
